dc7466cff567ec592ff125b72fd0ed453fe1dbf9
ethan102912 <fficehuang@gmail.com>
2022-10-05 18:52:13 +0800

    good

---
+++ class5/class/class.py
@@ -0,0 +1,24 @@
+# password = input("請輸入密碼:")
+# if password == "我是大帥逼":
+#     print("我知道")
+# elif password == "我太醜了":
+#     print("我知道")
+
+# else:
+#     print("太嫩了")
+
+score = float(input("請輸入成績"))
+if score >= 90:
+    print("A")
+elif score <= 89 and score >= 80:
+    print("B")
+elif score <= 79 and score >= 70:
+    print("C")
+elif score <= 69 and score >= 60:
+    print("D")
+elif score <= 59:
+    print("E")
+
+# git add .
+# git commit -m "提示文字"
+# git push

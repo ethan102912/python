88a99e83a5c6258b8aa3a0f4aba19e2a1d7f2072
ethan102912 <fficehuang@gmail.com>
2022-10-19 19:06:28 +0800

    class 6

---
+++ class5/HW/hw.py
@@ -0,0 +1,4 @@
+h = float(input("請輸入身高"))
+w = float(input("請輸入體重"))
+bmi = w / h**2
+print("你的bmi為" + str(bmi))
---
+++ class6/class.py
@@ -0,0 +1,22 @@
+import turtle as t
+# for i in range(100):
+#     t.forward(100)
+#     t.right(90)
+# t.done()
+#  import turtle
+# turtle.color('blue') # 設定小烏龜顏色
+# turtle.shape('turtle') # 設定小烏龜形'arrow', 'turtle',
+# #'circle', 'square','triangle', 'classic'.
+# turtle.stamp() # 蓋章
+# turtle.penup() # 提筆
+t.penup()
+t.shape('turtle')
+for i in range(
+        0,
+        100,
+):
+    t.forward(i)
+    t.right(25)
+    t.stamp()
+
+t.done()

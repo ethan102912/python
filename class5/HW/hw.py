82731dff9e543ea703e6e5db6b2914bd768f10d9
ethan102912 <fficehuang@gmail.com>
2022-10-25 21:48:55 +0800

    hi

--- class5/HW/hw.py
+++ class5/HW/hw.py
@@ -2,3 +2,10 @@ h = float(input("請輸入身高"))
 w = float(input("請輸入體重"))
 bmi = w / h**2
 print("你的bmi為" + str(bmi))
+
+if bmi >= 14.8 and bmi <= 20.7:
+    print("體重正常")
+elif bmi > 20.7:
+    print("體重過重")
+elif bmi < 14.8:
+    print("體重過輕")

3c89993799d1ee1ab7667a1f3a92c860de9b9503
ethan102912 <fficehuang@gmail.com>
2022-10-26 18:59:57 +0800

    hi

---
+++ class7/class7.py
@@ -0,0 +1,19 @@
+# import turtle as t
+
+# t.fillcolor()
+# t.begin_fill()
+# t.shape('turtle')
+# t.pensize(5)
+# t.pencolor("yellow")
+# t.fillcolor("yellow")
+# t.begin_fill()
+#     t.forward(200)
+#     t.right(144)
+
+# t.end_fill()
+# t.done()
+s = 0
+n = int(input('請輸入一個整數:'))
+for i in range(1, n + 1):
+    s = s + i
+print(s)
